cba8dcd15c389729a0696bad4a5a1584f4aee406
leonardomunoz <l.mnz.lpz@gmail.com>
2023-10-07 12:19:35 -0600

    Last test to check repo setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. Test to see everything is working correctly
         </a>
       </header>
     </div>
